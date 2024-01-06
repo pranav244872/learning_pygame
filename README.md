98c86d626f687dc54e6e947ca38f43636c271e4b
pranav244872 <aekbotepranav25@gmail.com>
2024-01-07 00:02:20 +0530

    Commit 3: game into an object

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ installed pygame with pip install pygame
 
 Commit 2:
 got the code running for a basic pygame empty window with 640,480 resolution
+
+Commit 3:
+learnt how to put game into an object
--- basics.py
+++ basics.py
@@ -1,15 +1,19 @@
 import pygame
 import sys
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('traffic')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit
 
-pygame.init()
-pygame.display.set_caption('traffic')
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit
+            pygame.display.update()
+            self.clock.tick(60)
 
-    pygame.display.update()
-    clock.tick(60)
+Game().run()
