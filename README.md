197f160af982a4a3281ba6ae1dc2534dc827bdd0
pranav244872 <aekbotepranav25@gmail.com>
2024-01-07 00:09:48 +0530

    Commit 4: imported image

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ got the code running for a basic pygame empty window with 640,480 resolution
 
 Commit 3:
 learnt how to put game into an object
+
+Commit 4:
+learnt how to draw an image in pygame
--- basics.py
+++ basics.py
@@ -6,8 +6,11 @@ class Game:
         pygame.display.set_caption('traffic')
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
+        self.img = pygame.image.load('obj.png')
+        self.img_pos = [160,260]
     def run(self):
         while True:
+            self.screen.blit(self.img, self.img_pos)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
